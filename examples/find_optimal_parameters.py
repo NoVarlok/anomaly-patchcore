765fb922d62aba35312d66f77056f99ea9b7f30b
NoVarlok <yakhtin.leonid@yandex.ru>
2023-05-10 01:57:13 +0300

    Add grid search over parameters

---
+++ examples/find_optimal_parameters.py
@@ -0,0 +1,112 @@
+import anomaly
+from anomaly.datasets import createDatasetDataloaders
+from anomaly.feature_extractor_wrappers import ResNetFeatureExtractor
+from anomaly.algorithms import Patchcore
+from anomaly.utils import seed_everything
+
+import numpy as np
+import pandas as pd
+import os
+import sklearn
+import argparse
+import torch
+import tarfile, io
+
+from sklearn.metrics import roc_auc_score
+from torchvision.models import resnet50, ResNet50_Weights
+from tqdm import tqdm
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Videos to images')
+    parser.add_argument('--dataset_dir', type=str)
+    parser.add_argument('--grid_search_output_tar', type=str,)
+    parser.add_argument('--output_report', type=str,)
+    parser.add_argument('--target_good_images', action='store_true')
+    args = parser.parse_args()
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    categories = [category for category in sorted(os.listdir(args.dataset_dir)) if os.path.isdir(os.path.join(args.dataset_dir, category))]
+
+    results = []
+    for use_pca in [True]:
+        for p_distance in [2.0]: #[0.1, 2.0, 5.0, 10.0]:
+            for n_patches in [1]: #[1, 3, 5, 10, 100]:
+                processed_categories = []
+                aurocs = []
+                for category in categories[:1]:
+                    seed_everything(42)
+                    print('CATEGORY:', category)
+                    train_loader, test_loader = createDatasetDataloaders(args.dataset_dir, category, 16)
+                    model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+                    feature_extractor = ResNetFeatureExtractor(model, device=device)
+                    config = {
+                        "coreset_sampling_ratio": 0.1,
+                        "n_patches": n_patches,
+                        "p_distance": p_distance,
+                        "PCA": use_pca,
+                    }
+                    patchcore = Patchcore(feature_extractor, config, device)
+                    
+                    patchcore.fit(train_loader)
+                    scores = []
+                    labels = []
+                    for i, data in enumerate(tqdm(test_loader)):
+                        # img, gt, label, idx = data
+                        img, label, idx = data
+                        score = patchcore.predict(img)
+                        labels.append(int(label[0]))
+                        scores.append(score)
+                    
+                    if args.target_good_images:
+                        labels = [label ^ 1 for label in labels]
+                    auroc = roc_auc_score(labels, scores)
+                    print('AUROC:', auroc)
+                    print()
+
+                    processed_categories.append(category)
+                    aurocs.append(auroc)
+                
+                df = pd.DataFrame(data={'class': ['avg'] + processed_categories,
+                                        'auroc': [np.mean(aurocs)] + aurocs})
+                
+                results.append({
+                    "avg_auroc": np.mean(aurocs),
+                    "use_pca": use_pca,
+                    "p_distance": p_distance,
+                    "n_patches": n_patches,
+                    "dataframe": df,
+                })
+
+                print("USE PCA:", use_pca)
+                print("P DISTANCE:", p_distance)
+                print("N PATCHES:", n_patches)
+                print(df.T.to_markdown())
+
+                results.sort(reverse=True, key=lambda x: x['avg_auroc'])
+
+                with open(args.output_report, "w") as fout:
+                    best_result = results[0]
+                    print("USE PCA:", best_result["use_pca"], file=fout)
+                    print("P DISTANCE:", best_result["p_distance"], file=fout)
+                    print("N PATCHES:", best_result["n_patches"], file=fout)
+                    print(best_result["dataframe"].T.to_markdown(), file=fout)
+    
+    with tarfile.open(args.grid_search_output_tar, "w:gz") as tar:
+        data = '\n\n'.join(
+            [f"""USE PCA: {result['use_pca']}
+P DISTANCE: {result['p_distance']}
+N PATCHES: {result['n_patches']}
+{result['dataframe'].T.to_markdown()}""" for result in results]
+        )
+        textIO = io.TextIOWrapper(io.BytesIO(), encoding='utf8')
+        textIO.write(data)
+        bytesIO = textIO.detach()
+        tarinfo = tarfile.TarInfo('results.txt')
+        tarinfo.size = bytesIO.tell()
+        bytesIO.seek(0)
+        tar.addfile(tarinfo, bytesIO)
+
+
+    
+
