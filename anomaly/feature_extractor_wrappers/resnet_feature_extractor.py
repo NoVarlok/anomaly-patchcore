33fc503509c9cf7a871f7c6dbd13900070327218
NoVarlok <yakhtin.leonid@yandex.ru>
2023-05-11 01:53:31 +0300

    Add custom conv kernel aggregation

--- anomaly/feature_extractor_wrappers/resnet_feature_extractor.py
+++ anomaly/feature_extractor_wrappers/resnet_feature_extractor.py
@@ -3,12 +3,32 @@ import torch.nn.functional as F
 from anomaly.feature_extractor_wrappers import BaseFeatureExtractor
 
 
+def get_kernel(output_channels, kernel_size=3):
+    margin = kernel_size//2
+    kernel = []
+
+    for i in range(kernel_size):
+        kernel_row = []
+        for j in range(kernel_size):
+            kernel_elem = 1 - max(abs(margin - i), abs(margin - j)) * 0.2
+            kernel_row.append(kernel_elem)
+        kernel.append(kernel_row)
+    kernel_sq = np.array(kernel)
+    kernel = np.zeros((output_channels, output_channels, kernel_size, kernel_size))
+    for i in range(output_channels):
+        kernel[i][i] = kernel_sq
+    return torch.tensor(np.float32(kernel))
+
+
 class ResNetFeatureExtractor(BaseFeatureExtractor):
-    def __init__(self, model, device, layers=[2, 3]):
+    def __init__(self, model, device, layers=[2, 3], aggregation_fn='avg'):
         super().__init__(model, device, layers)
+        self.aggregation_fn = aggregation_fn
+        self.out_size = [256, 512, 1024, 2048]
     
     def forward(self, x):
         features = []
+        kernels = []
         layers = [self.model.layer1, self.model.layer2,
                   self.model.layer3, self.model.layer4]
 
@@ -21,6 +41,10 @@ class ResNetFeatureExtractor(BaseFeatureExtractor):
             x = layer(x)
             if i + 1 in self.layers:
                 features.append(x)
+                kernels.append(get_kernel(self.out_size[i]))
         
-        features = [F.avg_pool2d(x, 3, 1, 1) for x in features]
+        if self.aggregation_fn == 'avg':
+            features = [F.avg_pool2d(x, 3, 1, 1) for x in features]
+        elif self.aggregation_fn == 'conv':
+            features = [f.conv2d(F.pad(x, ((0, 0), (0, 0), (1, 1), (1, 1)), "constant", 0), weight) for x, weight in zip(features, kernels)]
         return features
